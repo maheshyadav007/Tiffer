799314ab5fc609a2c10e6f21d251b6b8e60cfa46
mahesh yadav <myadav503@comcast.com>
2024-05-01 00:09:03 +0530

    refactoring and creating temp folder for repeated editing

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import time
+import shutil
 
-from utils import create_mask
+from utils import create_mask, run_inpaint_pipeline
 
-print("begin")
 # Specify canvas parameters in application
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
@@ -16,17 +17,23 @@ if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-print("selecting image")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-print("after selecting image")
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 
 if bg_image:
+    print("bg_image is not None")
     image = Image.open(bg_image)
     width, height = image.size
     width = int(width/2)
     height = int(height/2)
+    original_image_path = "./temp/original_image.png"
+    temp_image_path = "./temp/temp_image.png"
+    image.save(original_image_path)
+    shutil.copyfile(original_image_path, temp_image_path)
+    temp_image = Image.open(temp_image_path)
+
+
 else:
     width, height = 512, 512
 print(width, height)
@@ -39,7 +46,7 @@ canvas_result = st_canvas(
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
+    background_image=temp_image if bg_image else None,
     update_streamlit=realtime_update,
     height=height,
     width = width,
@@ -47,6 +54,7 @@ canvas_result = st_canvas(
     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
     key="canvas",
 )
+print(type(canvas_result))
 # print('after imgae selection')
 # Do something interesting with the image data and paths
 # if canvas_result.image_data is not None:
@@ -58,14 +66,14 @@ if canvas_result.json_data is not None:
     st.dataframe(objects)
 
 print(objects.columns)
-
-mask_origin_x = float(objects.left)
-mask_origin_y = float(objects.top)
-mask_width = float(objects.width)
-mask_height = float(objects.height)
-mask_angle = float(objects.angle)
-mask_scale_x = float(objects.scaleX)
-mask_scale_y = float(objects.scaleY)
+print(objects.left)
+mask_origin_x = objects.left.values[-1]
+mask_origin_y = objects.top.values[-1]
+mask_width = objects.width.values[-1]
+mask_height = objects.height.values[-1]
+mask_angle = objects.angle.values[-1]
+mask_scale_x = objects.scaleX.values[-1]
+mask_scale_y = objects.scaleY.values[-1]
 mask_width = mask_width * mask_scale_x
 mask_height = mask_height * mask_scale_y
 print(mask_origin_x, mask_origin_y, mask_width, mask_height, mask_angle)
@@ -85,39 +93,8 @@ mask = create_mask(height, width, origin_x, origin_y, mask_height, mask_width, m
 # st.image(mask, caption='Mask Image', width=width)
 # print(mask)
 
-from diffusers import StableDiffusionInpaintPipeline
-import torch
-import time
-
 
 text_input = st.text_input("Prompt to modify your image...")
-
-
-# Function to perform a specific task
-def perform_task():
-    # Perform your specific task here
-    # st.write("Task Completed!")
-    st.session_state.task_in_progress = True
-
-    # st.write("Button clicked with text:", text_input)
-    # .button("Running...", key = run_button_key)
-    pipe = StableDiffusionInpaintPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-2-inpainting",
-        torch_dtype=torch.float16,
-    )
-    pipe.to("mps")
-    prompt = text_input
-    #image and mask_image should be PIL images.
-    #The mask structure is white for inpainting and black for keeping as is
-    start = time.time()
-    output_image = pipe(prompt=prompt, image=image, mask_image=mask).images[0]
-    end = time.time()
-
-    image.save("./yellow_cat_on_park_bench.png")
-    st.image(output_image, width=width, caption=f"Inpaint Output. Time taken: {end-start:.2f} seconds")
-    st.session_state.task_in_progress = False
-    st.session_state.task_completed = True
-
 
 
 if 'task_in_progress' not in st.session_state:
@@ -127,7 +104,18 @@ if 'task_completed' not in st.session_state:
     st.session_state.task_completed = False
 
 if st.button("Run Inpainting"):
-    perform_task()
+    st.session_state.task_in_progress = True
+    start = time.time()
+    output_image = run_inpaint_pipeline(prompt=text_input, image=temp_image, mask=mask)
+    end = time.time()   
+    
+    st.image(output_image, width=width, caption=f"Inpaint Output. Time taken: {end-start:.2f} seconds")
+    st.session_state.output_image = output_image
+    st.session_state.task_in_progress = False
+    st.session_state.task_completed = True
+if st.button("Save"):
+    st.session_state.output_image.save(temp_image_path)
+    del st.session_state.output_image
 
 if st.session_state.task_in_progress:
     st.button("Running...")
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import math
+from diffusers import StableDiffusionInpaintPipeline
+import torch
+import streamlit as st
+import time
 
 def create_mask(height, width, origin_x, origin_y, mask_height, mask_width, mask_angle):
     mask = np.zeros((height, width), dtype=np.float32)
@@ -19,13 +23,13 @@ def create_mask(height, width, origin_x, origin_y, mask_height, mask_width, mask
         [mask_width, 0],
         [0 , 0]
     ], dtype=np.float32)
-    print(pts)
+    # print(pts)
     # Rotate each corner of the rectangle around the center
     rotated_pts = np.zeros_like(pts)
     for i in range(4):
         x = pts[i, 0] * 1*math.cos(angle_rad) - pts[i, 1] * 1*math.sin(angle_rad)
         y = pts[i, 0] * 1*math.sin(angle_rad) + pts[i, 1] * math.cos(angle_rad)
-        print(x,y)
+        # print(x,y)
         x = x + center_x
         y = y + center_y
         # if x<0:
@@ -35,8 +39,26 @@ def create_mask(height, width, origin_x, origin_y, mask_height, mask_width, mask
         rotated_pts[i] = [x , y ]
 
 
-    print(rotated_pts)
+    # print(rotated_pts)
     # Draw a filled polygon (the mask) on the array
     cv2.fillPoly(mask, [rotated_pts.astype(np.int32)], 1)
 
     return mask
+
+# Function to perform a specific task
+def run_inpaint_pipeline(prompt, image, mask):
+    # Perform your specific task here
+    # st.write("Task Completed!")
+
+    # st.write("Button clicked with text:", text_input)
+    # .button("Running...", key = run_button_key)
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-2-inpainting",
+        torch_dtype=torch.float16,
+    )
+    pipe.to("mps")
+    #image and mask_image should be PIL images.
+    #The mask structure is white for inpainting and black for keeping as is
+    output_image = pipe(prompt=prompt, image=image, mask_image=mask).images[0]
+    return output_image
+   
